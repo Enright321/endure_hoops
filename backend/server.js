deac733681237eb3acbec0183a58593d1196ae5d
Enright321 <enrightjoe4@gmail.com>
2022-03-11 00:43:14 -0600

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 const app = express();
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
