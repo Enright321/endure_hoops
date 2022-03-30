fa7d9141b011e399acec57d7ac6423afdde44ece
Enright321 <enrightjoe4@gmail.com>
2022-03-30 16:28:28 -0500

    logo update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -24,16 +24,32 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link onClick={() => setButtonPopup(true)} href='#'>
+              <Nav.Link
+                className='link'
+                onClick={() => setButtonPopup(true)}
+                href='#'
+              >
                 <i className='fa-solid fa-arrow-right-to-bracket'></i> Sign In
               </Nav.Link>
-              <Nav.Link href='#' onClick={() => setButtonPopup(true)}>
+              <Nav.Link
+                className='link'
+                href='#'
+                onClick={() => setButtonPopup(true)}
+              >
                 <i className='fa-solid fa-store'></i> Store
               </Nav.Link>
-              <Nav.Link href='#' onClick={() => setButtonPopup(true)}>
+              <Nav.Link
+                className='link'
+                href='#'
+                onClick={() => setButtonPopup(true)}
+              >
                 <i className='fa-solid fa-cart-shopping'></i> Cart
               </Nav.Link>
-              <Nav.Link href='#' onClick={() => setButtonPopup(true)}>
+              <Nav.Link
+                className='link'
+                href='#'
+                onClick={() => setButtonPopup(true)}
+              >
                 <i className='fa-solid fa-camera'></i> Gallery
               </Nav.Link>
             </Nav>
